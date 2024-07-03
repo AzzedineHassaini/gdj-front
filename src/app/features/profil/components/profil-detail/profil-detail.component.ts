150529aed105e03230f491c531e6ba51bfb587fc
Antoine Georis <antoinegeoris@outlook.be>
2024-07-03 12:16:47 +0200

    modif_feature_profil

--- src/app/features/profil/components/profil-detail/profil-detail.component.ts
+++ src/app/features/profil/components/profil-detail/profil-detail.component.ts
@@ -7,7 +7,74 @@ import { Component } from '@angular/core';
   styleUrl: './profil-detail.component.scss'
 })
 export class ProfilDetailComponent {
-  selectedFileImg: File | null = null;
+
+  selectedFiles: { [key: string]: File | null } = { profile: null, imprint: null };
+  imgPreview: { [key: string]: string | ArrayBuffer | null } = { profile: null, imprint: null };
+  uploadedimgUrl: { [key: string]: File | null } = { profile: null, imprint: null };
+
+  constructor(private http: HttpClient) {}
+
+  onFileSelected(event: Event, type: string): void {
+    const fileInput = event.target as HTMLInputElement;
+
+    if (fileInput.files && fileInput.files.length > 0) {
+      this.selectedFiles[type] = fileInput.files[0];
+
+      const reader = new FileReader();
+      reader.onload = () => {
+          this.imgPreview[type] = reader.result;
+        };
+
+
+      }
+    }
+  }
+
+  /*
+  selectedProfileFile: File | null = null;
+  profileImgPreview: string | ArrayBuffer | null = null;
+  uploadedProfileImgUrl: string | null = null;
+
+  selectedImprintFile: File | null = null;
+  imprintImgPreview: string | ArrayBuffer | null = null;
+  uploadedImprintImgUrl: string | null = null;
+
+  
+
+
+
+  onProfileImageSelected(event: Event): void {
+    const fileInput = event.target as HTMLInputElement;
+    if (fileInput.files && fileInput.files.length > 0) {
+      this.selectedProfileFile = fileInput.files[0];
+ 
+      // Prévisualiser l'image de profil
+      const reader = new FileReader();
+      reader.onload = () => {
+        this.profileImgPreview = reader.result;
+      };
+      reader.readAsDataURL(this.selectedProfileFile);
+    }
+  }
+ 
+  onImprintImageSelected(event: Event): void {
+    const fileInput = event.target as HTMLInputElement;
+    if (fileInput.files && fileInput.files.length > 0) {
+      this.selectedImprintFile = fileInput.files[0];
+ 
+      // Prévisualiser l'image d'empreinte
+      const reader = new FileReader();
+      reader.onload = () => {
+        this.imprintImgPreview = reader.result;
+      };
+      reader.readAsDataURL(this.selectedImprintFile);
+    }
+  }
+
+*/
+
+
+  /*selectedFileImg: File | null = null;
   selectedFileImprint: File | null = null;
   imgPreview: string | ArrayBuffer | null = null;
   imprintPreview: string | ArrayBuffer | null = null;
@@ -51,4 +118,3 @@ export class ProfilDetailComponent {
       })
     }
   }*/
-}
