205a5eaca6371875d00f20f17bb4845fdb46d2d9
Nicolasq <quinet.nicolas@gmail.com>
2024-07-10 13:39:32 +0200

    last corrction

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@angular/core';
-import {IAuth} from "../../features/auth/models/auth.model";
-import {BehaviorSubject, catchError, map, Observable, of, tap} from "rxjs";
-import {HttpClient} from "@angular/common/http";
-import {CookieService} from "ngx-cookie-service";
-import {ILoginForm} from "../../features/auth/form/login.form";
+import { IAuth } from "../../features/auth/models/auth.model";
+import { BehaviorSubject, catchError, map, Observable, of, tap } from "rxjs";
+import { HttpClient } from "@angular/common/http";
+import { CookieService } from "ngx-cookie-service";
+import { ILoginForm } from "../../features/auth/form/login.form";
 import { env } from '../../../env/env';
-import {IRegisterForm} from "../../features/auth/form/register.form";
-import {MessageService} from "primeng/api";
-import {TranslateService } from "@ngx-translate/core";
+import { IRegisterForm } from "../../features/auth/form/register.form";
+import { MessageService } from "primeng/api";
+import { TranslateService } from "@ngx-translate/core";
 
 
 @Injectable({
@@ -56,7 +56,7 @@ export class AuthService {
         this.currentUser = auth;
         this._message.add({
           severity: 'success',
-          summary: this._translate.instant('auth.titleloggedsucces'),
+          summary: this._translate.instant('auth.titleloggedsuccess'),
           detail: this._translate.instant('auth.successfullylogged')
         });
       }),
