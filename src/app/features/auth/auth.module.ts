e3f4611cb5bcf545c4a4535801f9f78915df872b
Nicolasq <quinet.nicolas@gmail.com>
2024-07-10 11:06:56 +0200

    message popup

--- src/app/features/auth/auth.module.ts
+++ src/app/features/auth/auth.module.ts
@@ -1,17 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './components/login/login.component';
-import {ReactiveFormsModule} from "@angular/forms";
-import {Button} from "primeng/button";
-import {TranslateModule} from "@ngx-translate/core";
-import {PasswordModule} from "primeng/password";
-import {InputTextModule} from "primeng/inputtext";
-import {InputGroupModule} from "primeng/inputgroup";
-import {InputGroupAddonModule} from "primeng/inputgroupaddon";
+import { ReactiveFormsModule } from "@angular/forms";
+import { Button } from "primeng/button";
+import { TranslateModule } from "@ngx-translate/core";
+import { PasswordModule } from "primeng/password";
+import { InputTextModule } from "primeng/inputtext";
+import { InputGroupModule } from "primeng/inputgroup";
+import { InputGroupAddonModule } from "primeng/inputgroupaddon";
 import { RegisterComponent } from './components/register/register.component';
-import {InputMaskModule} from "primeng/inputmask";
+import { InputMaskModule } from "primeng/inputmask";
+import { MessagesModule } from 'primeng/messages';
+import { MessageModule } from 'primeng/message';
+import { MessageService } from 'primeng/api';
 
 
 @NgModule({
@@ -29,7 +31,10 @@ import {InputMaskModule} from "primeng/inputmask";
     InputTextModule,
     InputGroupModule,
     InputGroupAddonModule,
-    InputMaskModule
-  ]
+    InputMaskModule,
+    MessagesModule,
+    MessageModule
+  ],
+  providers: [ MessageService]
 })
 export class AuthModule { }
--- src/app/features/auth/components/login/login.component.ts
+++ src/app/features/auth/components/login/login.component.ts
@@ -2,12 +2,14 @@ import { Component } from '@angular/core';
 import {FormBuilder, FormGroup} from "@angular/forms";
 import {AuthService} from "../../../../shared/services/auth.service";
 import {Router} from "@angular/router";
+import {MessageService} from "primeng/api";
 import {LOGIN_FORM} from "../../form/login.form";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
+  providers: [MessageService]
 })
 export class LoginComponent {
 
@@ -16,7 +18,8 @@ export class LoginComponent {
   constructor(
     builder: FormBuilder,
     private readonly _auth: AuthService,
-    private readonly _router: Router
+    private readonly _router: Router,
+    private readonly _message: MessageService
   ) {
     this.form = builder.group(LOGIN_FORM)
   }
@@ -25,16 +28,27 @@ export class LoginComponent {
     this._auth.login( this.form.value ).subscribe({
       next: (auth) => {
         console.log(this._auth.currentUser)
-        this._router.navigate(['home']);
+       
+        this._router.navigate(['home'])
+        
+        this._message.add({
+          severity: 'success',
+          summary: 'Auth success',
+          detail: 'You are successfully logged in'
+        })
+        console.log("test")
       },
       error: (error) => {
         console.log(error)
+        this._message.add({
+          severity: 'error',
+          summary: 'Auth failed',
+          detail: error.error
+        })
       },
       complete: () => {
 
       }
     })
-
   }
-
 }
